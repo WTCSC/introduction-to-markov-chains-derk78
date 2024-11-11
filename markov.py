1daae512291486d0eff002c6308e9532545b2409
Derek Williams <williamsdj503@gmail.com>
2024-11-11 10:58:32 -0700

    Markov Chain

--- markov.py
+++ markov.py
@@ -5,8 +5,9 @@ Create the sample text and the dictionary to store word transitions
 
 TODO: Replace the sample text with a larger text for more interesting results
 """
-text = "Mary had a little lamb its fleece was white as snow"
+text = "The practical thing was to find rooms in the city but it was a warm season and I had just left a country of wide lawns and friendly trees, so when a young man at the office suggested that we take a house together in a commuting town it sounded like a great idea. He found the house, a weather beaten cardboard bungalow at eighty a month, but at the last minute the firm ordered him to Washington and I went out to the country alone. I had a dog, at least I had him for a few days until he ran away, and an old Dodge and a Finnish woman who made my bed and cooked breakfast and muttered Finnish wisdom to herself over the electric stove."
 transitions = {}
+punctuation = {".","?","!",}
 
 """
 Build the Markov Chain
@@ -20,10 +21,13 @@ TODO: Handle punctuation and capitalization for better results
 words = text.split()
 for i in range(len(words) - 1):
     current_word = words[i]
+    #for char in current_word:
+        #make sure that if punctuation if found end the sentence from which word the punctuation was found 
     next_word = words[i + 1]
     if current_word not in transitions:
         transitions[current_word] = []
     transitions[current_word].append(next_word)
+    
 
 """
 Generate new text using the Markov Chain, starting with a given word and
@@ -42,19 +46,49 @@ e.g., capitalization, punctuation, line breaks, etc.
 """
 def generate_text(start_word, num_words):
     current_word = start_word
-    result = [current_word]
+    result = [current_word.capitalize()]
+    sentence_length = 0 
+    
     for _ in range(num_words - 1):
         if current_word in transitions:
             next_word = random.choice(transitions[current_word])
+            if sentence_length >= 5 and random.random() < 0.2:
+                result[-1] += "."
+                current_word = current_word.capitalize()
+                sentence_length = 1
+            elif random.random() < 0.1:
+                result[-1] += ","
+                
             result.append(next_word)
             current_word = next_word
+            sentence_length += 1 
         else:
             break
-    return " ".join(result)
+        
+    text_output = " ".join(result)
+    text_output = text_output.replace(". ", ".\n")  
+    return text_output
+
+
+user_input = input()
+print(generate_text(user_input, 100))
+    # for _ in range(num_words - 1):
+    #     if current_word in transitions:
+    #         next_word = random.choice(transitions[current_word])
+    #         result.append(next_word)
+    #         current_word = next_word
+    #     else:
+    #         break
+    # return " ".join(result)
 
 """
 Example usage, generating 10 words starting with "Mary"
 
 TODO: Accept user input for the starting word and number of words to generate
 """
-print(generate_text("Mary", 10))
+# user_input = input()
+# print(generate_text(user_input, 100))
+
+
+
+#make it so that It will accept however many characters I want it to generate.
